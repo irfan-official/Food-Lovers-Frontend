b3ed389f7f1f2f0d34ab874fa9fff3ef48dae73b
irfan-official <irfanofficial.work@gmail.com>
2025-11-13 21:10:24 +0600

    update AllReviews.jsx

--- src/pages/AllReviews.jsx
+++ src/pages/AllReviews.jsx
@@ -8,7 +8,7 @@ import { IoSearch } from "react-icons/io5";
 import { MdReviews } from "react-icons/md";
 
 function AllReviews() {
-  const { serviceData, loader } = useContext(Data_Context);
+  const { allReviews, loader } = useContext(Data_Context);
 
   const [searchApp, setSearchApp] = useState("");
   const [searchLoading, setSearchLoading] = useState(false);
@@ -64,7 +64,7 @@ function AllReviews() {
         >
           <h2 className="_apps-label_ text-[#632EE3] font-semibold flex items-center gap-2 text-[0.9rem] sm:text-[1rem]">
             <span className="px-4 py-2 bg-white rounded-md shadow font-extrabold">
-              {searchApp ? filteredApps.length : serviceData.length}
+              {searchApp ? filteredApps.length : allReviews.length}
             </span>
             <span className=" underline"> Reviews Found</span>
           </h2>
@@ -153,34 +153,34 @@ function AllReviews() {
             </p>
           )
         ) : (
-          serviceData.map(
+          allReviews.map(
             (
               {
-                serviceId,
-                serviceName,
-                providerName,
-                providerEmail,
-                price,
-                rating,
-                slotsAvailable,
-                description,
+                _id,
+                foodName,
                 image,
                 category,
+                ratings,
+                restaurantName,
+                location,
+                reviewText,
+                createdAt,
+                loved,
               },
               index
             ) => (
               <ReviewCard
-                key={index}
-                serviceId={serviceId}
-                serviceName={serviceName}
-                providerName={providerName}
-                providerEmail={providerEmail}
-                price={price}
-                rating={rating}
-                slotsAvailable={slotsAvailable}
-                description={description}
+                key={String(_id)}
+                reviewId={String(_id)}
+                foodName={foodName}
                 image={image}
                 category={category}
+                ratings={ratings}
+                restaurantName={restaurantName}
+                location={location}
+                reviewText={reviewText}
+                createdAt={createdAt}
+                loveCount={loved.length}
               />
             )
           )
